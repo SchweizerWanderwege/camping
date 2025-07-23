7720dfc7959bf0cbf7333cdcb47216d67d387f80
SchweizerWanderwege <92431739+SchweizerWanderwege@users.noreply.github.com>
2025-07-23 23:57:18 +0200

    Update Campingpltze_1_style.js

--- styles/Campingpltze_1_style.js
+++ styles/Campingpltze_1_style.js
@@ -1,7 +1,7 @@
 var style_Campingpltze_1 = function(feature, resolution){
     return [ new ol.style.Style({
         image: new ol.style.Icon({
-            src: "https://openlayers.org/en/latest/examples/data/icon.png"
+            src: "styles/campground-solid_green.svg"
         })
     }) ];
 };

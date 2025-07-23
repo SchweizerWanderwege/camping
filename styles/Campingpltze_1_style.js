78a4ee67630ad92bcc7cd1c39a6261f77090fb48
SchweizerWanderwege <92431739+SchweizerWanderwege@users.noreply.github.com>
2025-07-23 21:24:46 +0200

    Update Campingpltze_1_style.js

--- styles/Campingpltze_1_style.js
+++ styles/Campingpltze_1_style.js
@@ -13,7 +13,7 @@ function categories_Campingpltze_1(feature, value, size, resolution, labelText,
                   anchorXUnits: "pixels",
                   anchorYUnits: "pixels",
                   rotation: 0.0,
-                  src: "styles/campground-solid_green.svg"
+                  src: "styles/campground-solid_green.png"
             }),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
@@ -29,7 +29,7 @@ case '1.021':
                   anchorXUnits: "pixels",
                   anchorYUnits: "pixels",
                   rotation: 0.0,
-                  src: "styles/campground-solid_orange.svg"
+                  src: "styles/campground-solid_orange.png"
             }),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
